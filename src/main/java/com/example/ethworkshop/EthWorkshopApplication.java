197ae6dcdcfd6ed0460ec8c2958197310a104a6a
Lennart Kramer <lennart.kramer@42.nl>
2018-03-25 14:08:25 +0200

    first commit

---
+++ src/main/java/com/example/ethworkshop/EthWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.example.ethworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthWorkshopApplication.class, args);
+	}
+}
